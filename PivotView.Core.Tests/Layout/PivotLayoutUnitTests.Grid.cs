95f8a2f84b8bc669fb8964ee98fa2050b57bee86
Matthew Leibowitz <mattleibow@live.com>
2022-12-12 00:27:24 +0200

    grid layout

--- PivotView.Core.Tests/Layout/PivotLayoutUnitTests.Grid.cs
+++ PivotView.Core.Tests/Layout/PivotLayoutUnitTests.Grid.cs
@@ -65,7 +65,7 @@ public partial class PivotLayoutUnitTests
         }
 
         [Fact]
-        public void PerfectMultRowMultiColMeasureIsCorrect()
+        public void PerfectMultiRowMultiColMeasureIsCorrect()
         {
             var frame = new RectangleF(0, 0, 120, 120);
             var layout = new PivotGridLayout();
@@ -85,7 +85,7 @@ public partial class PivotLayoutUnitTests
         }
 
         [Fact]
-        public void MultRowMultiColMeasureIsCorrect()
+        public void MultiRowMultiColMeasureIsCorrect()
         {
             var frame = new RectangleF(0, 0, 120, 120);
             var layout = new PivotGridLayout();
@@ -105,7 +105,34 @@ public partial class PivotLayoutUnitTests
         }
 
         [Fact]
-        public void LayoutIsCorrect()
+        public void SingleItemArrangeIsCorrect()
+        {
+            var frame = new RectangleF(0, 0, 120, 120);
+            var layout = new PivotGridLayout();
+
+            var items = CreateItemList(new[] { "A" });
+
+            layout.LayoutItems(items, frame);
+
+            Assert.Equal(new(0, 0, 120, 120), items[0].Frame.Desired);
+        }
+
+        [Fact]
+        public void SingleRowArrangeIsCorrect()
+        {
+            var frame = new RectangleF(0, 0, 120, 120);
+            var layout = new PivotGridLayout();
+
+            var items = CreateItemList(new[] { "A", "B" });
+
+            layout.LayoutItems(items, frame);
+
+            Assert.Equal(new(0, 0, 60, 60), items[0].Frame.Desired);
+            Assert.Equal(new(60, 0, 60, 60), items[1].Frame.Desired);
+        }
+
+        [Fact]
+        public void ArrangeIsCorrect()
         {
             var frame = new RectangleF(0, 0, 120, 120);
             var layout = new PivotGridLayout();
@@ -114,9 +141,46 @@ public partial class PivotLayoutUnitTests
 
             layout.LayoutItems(items, frame);
 
-            Assert.Equal(new(0, 0, 120, 30), items[0].Frame.Desired);
-            Assert.Equal(new(0, 45, 120, 30), items[1].Frame.Desired);
-            Assert.Equal(new(0, 90, 120, 30), items[2].Frame.Desired);
+            Assert.Equal(new(0, 0, 60, 60), items[0].Frame.Desired);
+            Assert.Equal(new(60, 0, 60, 60), items[1].Frame.Desired);
+            Assert.Equal(new(0, 60, 60, 60), items[2].Frame.Desired);
+        }
+
+        [Fact]
+        public void PerfectMultiRowMultiColArrangeIsCorrect()
+        {
+            var frame = new RectangleF(0, 0, 120, 120);
+            var layout = new PivotGridLayout();
+
+            var items = CreateItemList(new[] { "A", "B", "C", "D", "E", "F" });
+
+            layout.LayoutItems(items, frame);
+
+            Assert.Equal(new(0, 0, 40, 40), items[0].Frame.Desired);
+            Assert.Equal(new(40, 0, 40, 40), items[1].Frame.Desired);
+            Assert.Equal(new(80, 0, 40, 40), items[2].Frame.Desired);
+            Assert.Equal(new(0, 40, 40, 40), items[3].Frame.Desired);
+            Assert.Equal(new(40, 40, 40, 40), items[4].Frame.Desired);
+            Assert.Equal(new(80, 40, 40, 40), items[5].Frame.Desired);
+        }
+
+        [Fact]
+        public void MultiRowMultiColArrangeIsCorrect()
+        {
+            var frame = new RectangleF(0, 0, 120, 120);
+            var layout = new PivotGridLayout();
+
+            var items = CreateItemList(new[] { "A", "B", "C", "D", "E", "F", "G" });
+
+            layout.LayoutItems(items, frame);
+
+            Assert.Equal(new(0, 0, 40, 40), items[0].Frame.Desired);
+            Assert.Equal(new(40, 0, 40, 40), items[1].Frame.Desired);
+            Assert.Equal(new(80, 0, 40, 40), items[2].Frame.Desired);
+            Assert.Equal(new(0, 40, 40, 40), items[3].Frame.Desired);
+            Assert.Equal(new(40, 40, 40, 40), items[4].Frame.Desired);
+            Assert.Equal(new(80, 40, 40, 40), items[5].Frame.Desired);
+            Assert.Equal(new(0, 80, 40, 40), items[6].Frame.Desired);
         }
     }
 }
--- PivotView.Core/Layout/PivotGridLayout.cs
+++ PivotView.Core/Layout/PivotGridLayout.cs
@@ -2,9 +2,23 @@
 
 public class PivotGridLayout : PivotLayout
 {
+    public enum LayoutOrigin
+    {
+        Top,
+        Bottom,
+    }
+
+    private enum LayoutDirection
+    {
+        LeftToRight = 1,
+        RightToLeft = -1
+    }
+
     public int Columns { get; protected set; }
 
     public int Rows { get; protected set; }
+
+    public LayoutOrigin Origin { get; set; }
 
     public override void Measure(IReadOnlyList<PivotRendererItem> items, RectangleF frame)
     {
@@ -50,8 +64,117 @@ public class PivotGridLayout : PivotLayout
 
     public override void Arrange(IReadOnlyList<PivotRendererItem> items, RectangleF frame)
     {
-        foreach (var item in items)
+        if (ItemWidth == 0 || ItemHeight == 0)
+            return;
+
+        var count = items.Count;
+
+        // determine the origin, direction and step values
+        double yOffset, yStep, xStep;
+        if (Origin == LayoutOrigin.Bottom)
         {
+            yOffset = frame.Height - ItemHeight;
+            yStep = ItemHeight * -1.0;
+            xStep = -ItemWidth;
+        }
+        else
+        {
+            yOffset = frame.Y;
+            yStep = ItemHeight;
+            xStep = ItemWidth;
+        }
+
+        // determine item actual size
+        var maxItemWidth = Math.Max(0.0, ItemWidth - ItemMargin * 2);
+        var maxItemHeight = Math.Max(0.0, ItemHeight - ItemMargin * 2);
+
+        // determine item list loop bounds
+        int currentIndex, stepIndex, lastIndex;
+        if (Origin == LayoutOrigin.Bottom)
+        {
+            currentIndex = items.Count - 1;
+            stepIndex = -1;
+            lastIndex = -1;
+        }
+        else
+        {
+            currentIndex = 0;
+            stepIndex = 1;
+            lastIndex = items.Count;
+        }
+
+        // build up the grid
+        for (var rowIndex = 0; rowIndex < Rows; rowIndex++)
+        {
+            // determine initial X offset
+            double xOffset;
+            if (Origin == LayoutOrigin.Bottom)
+            {
+                var partialCol = count % Columns;
+                xOffset = rowIndex == Rows - 1 && partialCol > 0
+                    ? ItemWidth * (partialCol - 1)
+                    : ItemWidth * (Columns - 1);
+            }
+            else
+            {
+                xOffset = frame.X;
+            }
+
+            for (var colIndex = 0; colIndex < Columns; colIndex++)
+            {
+                if (currentIndex == lastIndex)
+                    break;
+
+                var item = items[currentIndex];
+
+                // determine item aspect ratio
+                var itemAspect = item.AspectRatio;
+                if (double.IsNaN(itemAspect))
+                    itemAspect = 1.0;
+                // make the item fit in the space allotted
+                double width, height;
+                if (itemAspect > ItemAspectRatio)
+                {
+                    width = maxItemWidth;
+                    height = width / itemAspect;
+                }
+                else
+                {
+                    height = maxItemHeight;
+                    width = height * itemAspect;
+                }
+
+                // calculate item bounds
+                var newFrame = new RectangleF(
+                    (float)(xOffset + ItemMargin + (maxItemWidth - width) / 2.0),
+                    (float)(yOffset + ItemMargin + (maxItemHeight - height) / 2.0),
+                    (float)(width),
+                    (float)(height));
+
+                item.Frame.Desired = newFrame;
+
+                // step
+                xOffset += xStep;
+                currentIndex += stepIndex;
+
+                // find grid position
+                var itemRow = Origin == LayoutOrigin.Bottom
+                    ? Rows - 1 - rowIndex
+                    : rowIndex;
+                var itemCol = colIndex;
+                if (Origin == LayoutOrigin.Bottom)
+                {
+                    itemCol = Columns - 1 - colIndex;
+                    var partialCol = count % Columns;
+                    if (rowIndex == Rows - 1 && partialCol > 0)
+                        itemCol -= Columns - partialCol;
+                }
+
+                var gridPos = (itemRow, itemCol);
+            }
+
+            // step
+            yOffset += yStep;
         }
     }
 }
